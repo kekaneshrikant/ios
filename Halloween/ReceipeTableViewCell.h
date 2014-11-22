2992906da6e2fd9761d6d2f87a6fe5cae9c6a4d6
SourceKode <karanvora@Macintosh.local>
2014-11-22 18:56:25 +0530

    Updated the XCode with Receipe Detail View Controller

---
+++ Halloween/ReceipeTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ReceipeTableViewCell.h
+//  Halloween
+//
+//  Created by SourceKode on 22/11/14.
+//  Copyright (c) 2014 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReceipeTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *receipeImageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *receipeNameLabel;
+
+@end
---
+++ Halloween/ReceipeTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  ReceipeTableViewCell.m
+//  Halloween
+//
+//  Created by SourceKode on 22/11/14.
+//  Copyright (c) 2014 SK. All rights reserved.
+//
+
+#import "ReceipeTableViewCell.h"
+
+@implementation ReceipeTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

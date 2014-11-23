29643bede08d01150229acdae6d5233821c8940a
SourceKode <karanvora@Macintosh.local>
2014-11-23 18:30:21 +0530

    A new Communication Handler class is added.

---
+++ Halloween/CommunicationHandler.h
@@ -0,0 +1,15 @@
+//
+//  CommunicationHandler.h
+//  Halloween
+//
+//  Created by SourceKode on 23/11/14.
+//  Copyright (c) 2014 SK. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CommunicationHandler : NSObject
+
++(void) makeNetworkRequestForURLString:(NSString *)urlString;
+
+@end
